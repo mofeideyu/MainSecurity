fcd1f303bfc55f7b15e7704688134c370944d123
mofeisfish <mofei2914376031@163.com>
2022-04-17 10:17:08 +0800

    spring security Web 认证

---
+++ src/main/java/com/mofei/config/MvcConfigure.java
@@ -0,0 +1,15 @@
+package com.mofei.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**对springmvc进行自定义配置*/
+@Configuration
+public class MvcConfigure implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login.html").setViewName("login");
+        registry.addViewController("/index.html").setViewName("index");
+    }
+}
---
+++ src/main/java/com/mofei/config/SecurityConfigure.java
@@ -0,0 +1,49 @@
+package com.mofei.config;
+
+import com.mofei.service.MyUserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/***自定义security相关配置*/
+@Configuration
+public class SecurityConfigure extends WebSecurityConfigurerAdapter {
+    private MyUserDetailService myUserDetailService;
+    @Autowired
+    public void setMyUserDetailService(MyUserDetailService myUserDetailService) {
+        this.myUserDetailService = myUserDetailService;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder builder) throws Exception {
+        builder.userDetailsService(myUserDetailService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .mvcMatchers("/login.html").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                /**进行表单认证*/
+                .formLogin()
+                /**指定自定义登录页面*/
+                .loginPage("/login.html")
+                .loginProcessingUrl("/doLogin")
+                /**登录成功后跳转路径*/
+                .defaultSuccessUrl("/index.html",true)
+                /**登录失败，重定向到登录页面*/
+                .failureUrl("/login.html")
+                .and()
+                /**退出登录*/
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login.html")
+                .and()
+                /**关闭csrf*/
+                .csrf().disable();
+
+    }
+}
